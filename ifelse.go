b696ac7cb6938fe2ab9f00a95e12a06b59beb595
theboynextdoor <cman55245@gmail.com>
2025-05-24 10:59:34 +0530

    learning go

--- ifelse.go
+++ ifelse.go
@@ -10,4 +10,13 @@ func main() {
 	} else {
 		fmt.Println("Person is not adult")
 	}
+
+	role := "software developer"
+	hasPermission := false
+
+	if role == "software developer" && hasPermission {
+		fmt.Println("Given Person is a", role, "and has a permission")
+	} else {
+		fmt.Println("Person has no persmission to enter inside the panel")
+	}
 }
